2440e279614c90412870c662e34bd6c9485afe89
madhumitaamaharaj <106608359+madhumitaamaharaj@users.noreply.github.com>
2023-03-16 13:19:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-fxt6bm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-s2ip48)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from'react'
+export default function App() {
+
+  return (
+    <div className="App">
+      <label>Type Your Message Here</label>
+      <input className="input"type="text" maxlength={100}/>
+      </div>
+  );
+  }
+  
+
+  
